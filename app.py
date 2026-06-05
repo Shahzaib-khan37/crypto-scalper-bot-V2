74259431e3ed0cf05580375abf2ae266a675d7e3
ShahzaibKhan-cj <shahzaib.3736@gmail.com>
2026-06-05 11:38:02 +0500

    testv999

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, jsonify, request, send_from_directory
 import db
 import bot
 
+print("===== SHAHZAIB TEST V999 =====")
+
+
+
+
 # Initialize Flask App
 app = Flask(__name__, static_folder='static', static_url_path='')
 
